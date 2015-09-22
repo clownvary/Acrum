3b8ab09a647b3216224eddc5f9a44a91f666f782
clownvary <vary_007@163.com>
2015-09-23 00:03:05 +0800

    add layer

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -2,16 +2,27 @@
  * Created by clownvary on 2015/9/11.
  * Email:vary_007@163.com
  */
-angular.module('app', ['router','service.breadcrumbs'])
+angular.module('app', ['router', 'service.breadcrumbs'])
     //即使bread服务里是一个绑定的事件，只要注入了服务，就会全局执行，相当于全局绑定
 
-    .controller('home', ['$rootScope', '$scope', function ($rootScope, $scope,_bread_) {
+    .controller('home', ['$rootScope', '$scope', function ($rootScope, $scope, _bread_) {
         $rootScope.title = 'welcome';
         $scope.content = 'from scope';
-        $scope.curUser={};
-        $scope.curUser.isAdmin=true;
+        $scope.curUser = {};
+        $scope.curUser.isAdmin = true;
 
     }])
-    .controller('HeaderCtrl',['$scope','bread',function($scope,_bread_){
-       $scope.breads=_bread_.breads;//面包屑导航
+    .controller('HeaderCtrl', ['$scope', 'bread', function ($scope, _bread_) {
+        $scope.breads = _bread_.breads;//面包屑导航
+        $scope.login = function () {
+            layer.open({
+                type: 2,
+                title: 'test',
+                area:'500px',
+                offset:'auto',
+                content: './admin/user/admin.user.tpl.html',
+                icon:3
+
+            });
+        }
     }]);
